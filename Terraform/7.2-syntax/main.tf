e02311cdb423577a14d0993a1a9ec92e9b212521
Svetlana Aksyonova <aksyonova.sa@gmail.com>
2022-06-12 23:54:08 +0300

    7.2-Terraform-syntax

---
+++ Terraform/7.2-syntax/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers { 
+    yandex = { 
+      source = "yandex-cloud/yandex" 
+    } 
+  } 
+  required_version = ">= 0.13" 
+} 
+
+provider "yandex" { 
+  token     = var.token 
+  cloud_id  = "b1glptti2hjffghqmm9n" 
+  folder_id = "b1g0rt8dea8afd5hqpnr" 
+  zone      = "ru-central1-a" 
+} 
+
+variable "token" {}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_vpc_network" "net" {
+  name = "net"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet"
+  network_id     = resource.yandex_vpc_network.net.id
+  v4_cidr_blocks = ["10.2.0.0/16"]
+  zone           = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm" {
+  name        = "netology"
+  hostname    = "netology.local"
+  platform_id = "standard-v1"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 100
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      type     = "network-hdd"
+      size     = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+    ipv6      = false
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
